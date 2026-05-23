9824bb4941ba0a9667493f4a396be63e2110c1af
Muhammadziyo <muhammadziyo792@gmail.com>
2026-05-23 13:26:30 +0500

    Use new database

--- backend/app/database.py
+++ backend/app/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./mentor.db"
+SQLALCHEMY_DATABASE_URL = "sqlite:///./mentor_new.db"
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
