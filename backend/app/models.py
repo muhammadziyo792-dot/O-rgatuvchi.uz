86bfa9b2ec8931ad9fab18b9bc1a29e7c9184f5c
Muhammadziyo <muhammadziyo792@gmail.com>
2026-05-29 16:31:07 +0500

    Fix User index

--- backend/app/models.py
+++ backend/app/models.py
@@ -24,7 +24,7 @@ class Teacher(Base):
 class User(Base):
     __tablename__ = "users"
 
-    id = Column(Integer, primary_key=True, index=True)
+    id = Column(Integer, primary_key=True)
     full_name = Column(String(100), nullable=False)
     email = Column(String(100), nullable=False, unique=True)
     hashed_password = Column(String(200), nullable=False)
